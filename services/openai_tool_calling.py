e620b4f9c09eb42190a5378c20524182c3da37c2
Daniel Stewart <daniel.stewart77@gmail.com>
2025-03-15 10:20:04 -0500

    1) added tool calling for ollama and openai 2) added tool wrapper for openai

--- services/openai_tool_calling.py
+++ services/openai_tool_calling.py
@@ -3,12 +3,18 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
-from services.agent_tooling import get_tools
+from services.openai_tool_wrapper import get_tools
 
 load_dotenv(dotenv_path='secrets.env')
 
 
 def call_tools_openai(messages: list[dict[str, str]]) -> dict:
+
+    messages.append({
+        "role": "system",
+        "content": f'''Choose one or multiple tools that would be useful for solving the task.'''
+    })
+
     """Interprets a user query and returns a standardized response dict."""
     client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
@@ -33,10 +39,16 @@ def call_tools_openai(messages: list[dict[str, str]]) -> dict:
             # Tool functions now return standardized responses
             result = function_to_call(**args)
             messages.append({
-                "role": "system",
+                "role": "function",
+                "tool_call_id": tool_call.id,
+                "name": name,
                 "content": result
             })
-            return messages
+        
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages
+    )
 
-    # Direct LLM response with no tool call
-    return {"response": completion.choices[0].message.content}
+    content = completion.choices[0].message.content
+    return content
